2c1dc3367a17b2af5fc0ffe5706656a4744429aa
Yamnyr <quentinwaroquet@gmail.comm>
2024-03-19 17:22:29 +0100

    commentaires

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -8,24 +8,27 @@ function Todolist() {
     const [todos, setTodos] = useState(() => {
         const storedTodos = localStorage.getItem("todos");
         return storedTodos ? JSON.parse(storedTodos) : [];
-    }); // State pour stocker les todos
+    });
+    // State pour stocker la liste des todos (recupère les to do du local storage si le usestate est vide
 
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
+    // met a jour le local storage des todos des que le usestate de to do est mis a jour
 
     const handleInputChange = (event) => {
-        setInput(event.target.value); // Mettre à jour la valeur de l'input
+        setInput(event.target.value);                 // Mettre à jour la valeur de l'input
     };
 
     const addTodo = () => {
         if (input.trim() !== "") {
-            setTodos([...todos, input]); // Ajouter le contenu de l'input aux todos
-            setInput(""); // Effacer l'input après l'ajout
+            setTodos([...todos, input]);        // Ajouter le contenu de l'input aux todos
+            setInput("");                       // Effacer l'input après l'ajout
         }
     };
 
     const removeTodo = (indexToRemove) => {
+        // Retrouve le to do dans la liste des todos via l'index et l'extrait de la liste des todos avec filter
         const updatedTodos = todos.filter((_, index) => index !== indexToRemove);
         setTodos(updatedTodos);
     };
@@ -44,8 +47,10 @@ function Todolist() {
                     variant="outlined"
                     value={input}
                     onChange={handleInputChange} // Utiliser la fonction de gestionnaire d'événements pour mettre à jour l'input
+                    inputProps={{ style: { color: '#fdf0d5' } }}
                 />
                 <Button
+                    className={"valid"}
                     variant="contained"
                     onClick={addTodo} // Utiliser la fonction d'ajout de todo
                 ><AddIcon />
@@ -61,7 +66,7 @@ function Todolist() {
                         <Button
                             className={"delete"}
                             variant="contained"
-                            onClick={() => removeTodo(index)}
+                            onClick={() => removeTodo(index)} // Utiliser la fonction de supression de todo
                         ><DeleteOutlineIcon />
                         </Button>
                     </div>
